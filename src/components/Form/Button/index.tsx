6bca14b8d5663f04268cd0be34075362ec6cd4e0
DilanAtomica <dilan-shwane@hotmail.com>
2023-02-09 10:56:39 +0100

    revamped the design of the post modal

--- src/components/Form/Button/index.tsx
+++ src/components/Form/Button/index.tsx
@@ -6,18 +6,11 @@ type buttonProps = {
     buttonType: "button" | "submit",
     disabled: boolean,
     width: string,
-    setSeason?: (seasonNumber: number) => void,
-    seasonNumber?: number,
 }
 
-function Button({children, buttonType, disabled, width, setSeason, seasonNumber} : buttonProps) {
-
-    const handleOnClick = () => {
-        if(setSeason && seasonNumber) setSeason(seasonNumber);
-    }
-
+function Button({children, buttonType, disabled, width} : buttonProps) {
     return (
-        <button onClick={handleOnClick} role={"submitBtn"} style={{opacity: disabled ? "0.5" : "1", width: width}} disabled={disabled} id="button" type={buttonType}>
+        <button role={"submitBtn"} style={{opacity: disabled ? "0.5" : "1", width: width}} disabled={disabled} id="button" type={buttonType}>
             {children}
         </button>
     );
--- src/pages/Searched/components/SeriesModal/index.tsx
+++ src/pages/Searched/components/SeriesModal/index.tsx
@@ -5,34 +5,42 @@ import {useSeriesResult} from "./hooks/api"
 import useSeriesModal from "../../../../stores/SeriesModal";
 import Button from "../../../../components/Form/Button";
 import LoadingScreen from "../../../../components/LoadingScreen";
+import { v4 as uuidv4 } from 'uuid';
 
 function SeriesModal() {
 
     const seriesModal = useSeriesModal();
     const {data, isFetching} = useSeriesResult(seriesModal.seriesID);
 
-    const [season, setSeason] = useState(1);
+    const [season, setSeason] = useState<number | null>(null);
+    const [episodeInput, setEpisodeInput] = useState<string | null>(null);
     const [episodes, setEpisodes] = useState<number[] | null>(null);
 
     useEffect(() => {
-        let episodeCount = null;
-        for(let i = 0; i < data?.seasons.length; i++) {
-            if(data.seasons[i].season_number === season) {
-                episodeCount = data.seasons[i].episode_count;
+        if(season) {
+            let episodeCount = null;
+            for(let i = 0; i < data?.seasons.length; i++) {
+                if(data.seasons[i].season_number === season) {
+                    episodeCount = data.seasons[i].episode_count;
+                }
             }
+            let episodeList = [];
+            for(let i = 1; i < episodeCount + 1; i++) {
+                episodeList.push(i);
+            }
+            console.log("Useeffect: Series Modal")
+            setEpisodes(episodeList);
         }
-        let episodeList = [];
-        for(let i = 1; i < episodeCount + 1; i++) {
-            episodeList.push(i);
-        }
-        console.log("Useeffect: Series Modal")
-        setEpisodes(episodeList);
     }, [season, data]);
 
     const onBackgroundClick = (e: React.MouseEvent<HTMLElement>) => {
         if (e.target instanceof HTMLElement) {
             if(e.target.classList.contains("seriesModalContainer")) seriesModal.deActivateSeriesModal();
         }
+    };
+
+    const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
     }
 
     return (
@@ -47,20 +55,26 @@ function SeriesModal() {
                 <div className="seriesContent">
                     <img src={data?.backdrop_path ? "https://image.tmdb.org/t/p/w500" + data.backdrop_path : "https://upload.wikimedia.org/wikipedia/commons/b/b1/Missing-image-232x150.png"}
                          alt={data?.name} />
-                    <div className="seasons">
-                        {data?.seasons.map((season: any) => (
-                            season.name !== "Specials" &&
-                            <Button setSeason={setSeason} seasonNumber={season.season_number} key={season.id}
-                                    buttonType={"button"} disabled={false} width={"max-content"}>{season.name}</Button>
-                        ))}
-                    </div>
-                    <h2>Episodes</h2>
-                    <div className="episodes">
-                        {episodes?.map((episode: any) => (
-                            <Button key={episode}
-                                    buttonType={"button"} disabled={false} width={"max-content"}>{episode}</Button>
-                        ))}
-                    </div>
+
+                    <form onSubmit={handleOnSubmit}>
+                        <label htmlFor="episodes">Choose a season:</label>
+                        <select onChange={(e) => setSeason(parseInt(e.target.value))} name="seasons" id="seasons">
+                            {data?.seasons.map((season: any) => (
+                                season.name !== "Specials" &&
+                                <option key={uuidv4()} value={season.season_number}>Season {season.season_number}</option>
+                            ))}
+                        </select>
+
+
+                        <label htmlFor="episodes">Choose an episode:</label>
+                        <select disabled={season === null} onChange={(e) => setEpisodeInput(e.target.value)} name="episodes" id="episodes">
+                            {episodes?.map((episode: any) => (
+                            <option key={uuidv4()} value={episode}>Episode {episode}</option>
+                            ))}
+                        </select>
+                        <Button buttonType={"submit"} disabled={false} width={"max-content"}>Find a chatter</Button>
+                    </form>
+
                 </div>
             </div>
 
