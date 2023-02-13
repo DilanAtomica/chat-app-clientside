9b39211d97c899f935de914f46b5ad706ab6a258
DilanAtomica <dilan-shwane@hotmail.com>
2023-02-13 20:10:19 +0100

    added navigation buttons in navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import "./index.css";
 import Logo from "../../images/logo.jpg";
+import {useNavigate} from "react-router-dom";
+import {AiOutlineWechat} from "react-icons/ai";
+import {FaUserAlt} from "react-icons/fa"
+import {AiFillHome} from "react-icons/ai";
 
 
 function Navbar() {
+
+    const navigate = useNavigate();
+
     return (
         <nav>
-            <img alt="logo" src={Logo} />
+            <button><img onClick={() => navigate("/home")} alt="logo" src={Logo} /></button>
+            <ul>
+                <li><button><AiOutlineWechat onClick={() => navigate("/chat")} className="navIcon" /></button></li>
+                <li><button><FaUserAlt onClick={() => navigate("/profile")} className="navIcon" /></button></li>
+                <li><button><AiFillHome onClick={() => navigate("/home")} className="navIcon" /></button></li>
+            </ul>
         </nav>
     );
 }
