2085518f853bb75e53b745f711e7aac3ca94b3ed
DilanAtomica <dilan-shwane@hotmail.com>
2023-04-27 08:12:53 +0200

    changed back from hasrouter to browserrouter

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {createHashRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
 import PublicRoutes from "./PublicRoutes";
 import ProtectedRoutes from "./ProtectedRoutes";
 
@@ -12,7 +12,7 @@ const Chat = React.lazy(() => import("../pages/Chat"));
 
 function AppRoutes() {
 
-    const router = createHashRouter(
+    const router = createBrowserRouter(
         createRoutesFromElements(
             <Route errorElement={<h1>There is nothing to see here...</h1>}>
                 <Route element={<PublicRoutes/>}>
